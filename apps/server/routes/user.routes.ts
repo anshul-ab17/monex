07cfb31cbfd80728584de235a33d495b8db65d2f
Anshul Bharat <anshul.ab17x@gmail.com>
2026-06-11 10:43:07 +0530

    added ledger_schema -> source of truth

--- apps/server/routes/user.routes.ts
+++ apps/server/routes/user.routes.ts
@@ -3,13 +3,13 @@ import type {FastifyInstance } from "fastify";
 export async function userRoutes(
   app: FastifyInstance
 ) {
-  app.get("/api/user/me", async () => {
+  app.get("user/me", async () => {
     return {
       message: "Current user",
     };
   });
 
-  app.patch("/api/user/me", async () => {
+  app.patch("user/me", async () => {
     return {
       message: "Update profile",
     };
