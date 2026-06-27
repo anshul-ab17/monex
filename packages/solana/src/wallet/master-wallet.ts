9c5cca019cafa2b1992206e3245aceea71ddaad3
Anshul Bharat <anshul.ab17x@gmail.com>
2026-06-27 22:27:11 +0530

    feat(wallet): refactor getMasterWallet

--- packages/solana/src/wallet/master-wallet.ts
+++ packages/solana/src/wallet/master-wallet.ts
@@ -2,9 +2,7 @@ import { Keypair } from "@solana/web3.js";
 import bs58 from "bs58";
 
 export function getMasterWallet() {
-    return Keypair.fromSecretKey(
-        bs58.decode(
-        process.env.SOLANA_MASTER_SECRET!,
-        ),
-    );
+    const secret = process.env.SOLANA_MASTER_SECRET;
+    if (!secret) throw new Error("SOLANA_MASTER_SECRET env var not set");
+    return Keypair.fromSecretKey(bs58.decode(secret));
 }
