ebb9bf04f7785f877eb4ae7ea9aaaf92884164a4
Anshul Bharat <anshul.ab17x@gmail.com>
2026-06-30 02:34:25 +0530

    feat(redis): export Redis constructor for subscriber instances
    Export Redis class from @repo/redis so consumers can create
    dedicated subscriber connections without importing ioredis directly.

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,5 +1,1 @@
-export * from "./src/cache";
-export * from "./src/client";
-export * from "./src/keys";
-export * from "./src/nonce";
-export * from "./src/session";
+export * from "./src/index";
--- packages/redis/src/client.ts
+++ packages/redis/src/client.ts
@@ -1,8 +1,11 @@
 import Redis from "ioredis";
 
+export { Redis };
+
 export const redis = new Redis(
     process.env.REDIS_URL!,{
         lazyConnect: true,
         maxRetriesPerRequest:3
     }
 );
+
