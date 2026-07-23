ee38dc8fd024b624c9ba403c5bcfcfd08ce9d90f
Anshul Bharat <anshul.ab17x@gmail.com>
2026-07-23 10:15:30 +0530

    chore: update project configuration and container definitions for reishi

--- packages/kafka/src/client.ts
+++ packages/kafka/src/client.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 import { env } from "@repo/config";
 
 export const kafka = new Kafka({
-    clientId: "monex",
+    clientId: "reishi",
     brokers: [env.KAFKA_BROKER],
     requestTimeout: 30000,
 });
